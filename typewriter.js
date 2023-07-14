9abb8eddc8ba10a478e282717e00564fbe308e91
Taha Mulla <tahamulla@hotmail.com>
2023-07-14 14:52:55 -0400

    eslint code

--- typewriter.js
+++ typewriter.js
@@ -3,7 +3,7 @@ const sentence = "hello there from lighthouse labs";
 let delay = 0;
 for (const char of sentence) {
   setTimeout(() => {
-    // console.log will add a new line character (\n) to the end of the string. 
+    // console.log will add a new line character (\n) to the end of the string.
     // However, in Node we can use process.stdout.write instead to have more control and avoid automatically adding an extra "newline character" at the end each time.
     // console.log(char);
     process.stdout.write(char);
